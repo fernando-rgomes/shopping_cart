c69354763967c3488956e527ddc22d4b2734940e
Fernando <166315110+fernando-rgomes@users.noreply.github.com>
2025-07-31 22:35:14 -0300

    Enhance cart service with display and improved removal
    Adicionada a função displayCart para mostrar o conteúdo do carrinho e melhorada a lógica removeItem para lidar com a diminuição da quantidade e a exclusão de itens. Atualizado o index.js para usar as novas funções do serviço de carrinho e proporcionar uma experiência de carrinho de compras mais interativa.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
+import * as cartService from "./services/cart.js"
 import createItem from "./services/item.js";
 
-const cart = [];
+const myCart = [];
+const myWishList = [];
 const item1 = await createItem("Bermuda lacoste", 30.99, 1);
 const item2 = await createItem("Camisa nike", 40.00, 2);
 
-console.log(item2.subtotal());
+console.log("Welcome to Shopping Cart!\n")
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+
+await cartService.removeItem(myCart, item2);
+await cartService.removeItem(myCart, item2);
+
+await cartService.displayCart(myCart);
+// await cartService.deleteItem(myCart, item1.name)
+// await cartService.deleteItem(myCart, item)
+
+
+
+await cartService.calculateTotal(myCart);
--- src/services/cart.js
+++ src/services/cart.js
@@ -6,22 +6,55 @@ async function addItem(userCart, item){
 }
 // -> deletar item do carrinho 
 async function deleteItem(userCart, name){
+    const index = userCart.findIndex((item)=> item.name === name);
 
+    if(index !== -1){
+        userCart.splice(index, 1);
+    }
 }
 // -> remover um item - diminui um item
-async function removeItem(userCart, index){
+async function removeItem(userCart, item){
+    //1. encontrar o indice do item
+   const indexFound = userCart.findIndex((p) => p.name === item.name);
 
+   //2. caso não encontre o item
+   if (indexFound === -1){
+    console.log("item não encontrado");
+    return;
+   }
+
+   //3. item >1 subtrair um item
+   if(userCart[indexFound].quantity > 1){
+    userCart[indexFound].quantity -= 1
+    return;
+   }
+
+   //4. caso item = 1 deletar o item
+   if(userCart[indexFound].quantity ===1){
+    userCart.splice(indexFound, 1);
+    return;
+   }
 }
 
 // -> calcular o total
 async function calculateTotal(userCart){
-    return userCart.reduce((total, item)=> total + item.subtotal(), 0);
+    console.log("\nShopping Cart TOTAL IS:  ")
+
+    const result = userCart.reduce((total, item)=> total + item.subtotal(), 0);
+    console.log(`Total: ${result}`);
 }
 
+async function displayCart(userCart){
+    console.log("Shopping Cart list: ");
+    userCart.forEach((item, index) =>{
+        console.log(`${index +1}. ${item.name} - R$ ${item.price} | ${item.quantity}x | Subtotal ${item.subtotal()}`);
+    });
+}
 
 export {
     addItem, 
     deleteItem,
     removeItem,
-    calculateTotal
+    calculateTotal,
+    displayCart
 }
